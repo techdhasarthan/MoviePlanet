b57a35bd6fd311c5a38d53e5c8c3e46a6905be36
techdhasarthan <dhasarathandharmaraj@gmail.com>
2023-05-24 20:19:26 +0530

    update api

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import queryString from "query-string";
 
-// const baseURL = "https://moonflix-api.vercel.app/api/v1/";
-const baseURL = "http://127.0.0.1:5000/api/v1/";
+const baseURL = "https://moonflix-api-sand.vercel.app/api/v1/";
+//const baseURL = "http://127.0.0.1:5000/api/v1/";
 
 
 const publicClient = axios.create({
